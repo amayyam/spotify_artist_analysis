c9a90453604a4c4c15cb07638e44f0f8f332a68c
amayyam <118068917+amayyam@users.noreply.github.com>
2024-10-10 17:35:38 -0400

    Update 02-download_data.R

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Spotify
+# Author: Maryam Ansari
+# Date: 10 October 2024
+# Contact: mayyam.ansari@mail.utornto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None.
+# Any other information needed? None.
 
 
 #### Workspace setup ####
@@ -16,18 +16,10 @@ library(spotifyr)
 
 #### Download data ####
 radiohead <- get_artist_audio_features("radiohead")
-rihanna <- get_artist_audio_features("rihanna")
 saveRDS(radiohead, "radiohead.rds")
-saveRDS(rihanna, "rihanna.rds")
 
 radiohead <- readRDS("radiohead.rds")
-rihanna <- readRDS("rihanna.rds")
-
-view(rihanna)
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
 write_csv(radiohead , "~/Desktop/STA304/spotify_artist_analysis/data/01-raw_data/raw_data.csv") 
-
          
