a96a7ddc6d61c1b623a9bbd72c91910e3de41716
amayyam <118068917+amayyam@users.noreply.github.com>
2024-10-10 17:36:07 -0400

    Update 03-clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,24 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Spotify
+# Author: Maryam Ansari
+# Date: 10 October 2024
+# Contact: mayyam.ansari@mail.utornto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Download Data
+# Any other information needed? None.
 
-#### Workspace setup ####
-library(tidyverse)
+library(dplyr)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+# Load the data
+radiohead_data <- read.csv('~/Desktop/STA304/spotify_artist_analysis/data/01-raw_data/raw_data.csv')
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Convert duration from ms to minutes
+radiohead_data <- radiohead_data %>%
+  mutate(duration_min = duration_ms / 60000)
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+# Select relevant columns for analysis
+cleaned_radiohead_data <- radiohead_data %>%
+  select(artist_name, album_name, track_number, duration_min, acousticness, danceability, energy, instrumentalness, valence, tempo)
+
+# Save cleaned data
+write_csv(cleaned_radiohead_data, "~/Desktop/STA304/spotify_artist_analysis/data/02-analysis_data/analysis_data.csv")
